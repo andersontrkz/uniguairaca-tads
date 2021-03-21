d1af59746f92b7f86127457f2df1163ad1d4d3bd
Anderson <trkz.anderson@gmail.com>
2021-03-21 13:09:23 -0300

    Atividade Calculadora Simples Completa

---
+++ analise-projetos-e-desenvolvimento-de-sistemas/atividade-01-calculadora-simples/src/controller/Operacao.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package controller;
+
+/**
+ *
+ * @author administrador
+ */
+public class Operacao {
+    public static int adicao(int numero1, int numero2){
+        return numero1 + numero2;
+    }
+    
+    public static int subtracao(int numero1, int numero2){
+        return numero1 - numero2;
+    }
+    
+    public static int multiplicacao(int numero1, int numero2){
+        return numero1 * numero2;
+    }
+    
+    public static int divisao(int numero1, int numero2){
+            return numero1 / numero2;
+    }
+}
